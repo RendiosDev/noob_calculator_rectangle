c958537e357caf6fba2f702ad49d58d9dcc84512
RendiosDev <121746202+RendiosDev@users.noreply.github.com>
2024-02-12 19:53:16 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+import time
+print('Calculator! Circuit of Rectangle!')
+first = input('Type first number of the side of the rectangle: ')
+second = input('Type second number of the side of the rectangle: ')
+number = int(first + second)
+final = int(number * 2)
+print('Final number is...')
+print(final)
+time.sleep(10)
